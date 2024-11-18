56baf964a801592bdfcd983abd30594423ef6a62
Zoezouzy <zoezouzy@gmail.com>
2024-11-17 23:10:30 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+/* ========== typing animation ========== */
+var typed = new Typed(".typing",{
+    strings:["Product Manager","Product Marketer", "Web Developer", "Graphic Designer"],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop:true
+})
